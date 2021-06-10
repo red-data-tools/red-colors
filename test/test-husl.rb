b61fb8fe81404786618708f31010bd4ba7ad9ade
Kenta Murata <mrkn@mrkn.jp>
2021-06-10 15:38:43 +0900

    test: Add test cases to check conversion

--- test/test-husl.rb
+++ test/test-husl.rb
@@ -185,4 +185,49 @@ class ColorsHUSLTest < Test::Unit::TestCase
     assert_same(black, black.to_hsl)
   end
 
+  sub_test_case("basic colors") do
+    colors = [
+      { rgb: "#ff0000", husl: [ 12.16, 1.0, 0.532], name: "red" },
+      { rgb: "#ffff00", husl: [ 85.90, 1.0, 0.971], name: "yellow" },
+      { rgb: "#00ff00", husl: [127.72, 1.0, 0.877], name: "green" },
+      { rgb: "#00ffff", husl: [192.20, 1.0, 0.911], name: "cyan" },
+      { rgb: "#0000ff", husl: [265.87, 1.0, 0.323], name: "blue" },
+      { rgb: "#ff00ff", husl: [307.71, 1.0, 0.603], name: "magenta" },
+    ]
+    data(colors.map {|r| [r[:name], r] }.to_h)
+    def test_husl_to_rgb(data)
+      husl, rgb = data.values_at(:husl, :rgb)
+      assert_equal(rgb,
+                   Colors::HUSL.new(*husl).to_rgb.to_hex_string)
+    end
+  end
+
+  sub_test_case("reproducing color conversion in seaborn") do
+    s = 0.9 * 99/100r
+    l = 0.65 * 99/100r
+    palette = [
+      { rgb: "#f77189", husl: [  3.590,            s, l] },
+      { rgb: "#e68332", husl: [ 33.50666666666666, s, l] },
+      { rgb: "#bb9832", husl: [ 63.42333333333333, s, l] },
+      { rgb: "#97a431", husl: [ 93.3400,           s, l] },
+      { rgb: "#50b131", husl: [123.25666666666666, s, l] },
+      { rgb: "#34af84", husl: [153.17333333333332, s, l] },
+      { rgb: "#36ada4", husl: [183.0900,           s, l] },
+      { rgb: "#38aabf", husl: [213.00666666666663, s, l] },
+      { rgb: "#3ba3ec", husl: [242.92333333333332, s, l] },
+      { rgb: "#a48cf4", husl: [272.8400,           s, l] },
+      { rgb: "#e867f4", husl: [302.75666666666666, s, l] }, # TODO: #e866f4 is the best
+      { rgb: "#f668c2", husl: [332.67333333333335, s, l] }
+    ]
+    data(palette.map {|r|
+      name = r[:name] || "husl(#{r[:husl]}) => #{r[:rgb]}"
+      [name, r]
+    }.to_h)
+    def test_husl_to_rgb(data)
+      husl_components, hex_string = data.values_at(:husl, :rgb)
+      husl = Colors::HUSL.new(*husl_components)
+      assert_equal(hex_string,
+                   husl.to_rgb.to_hex_string)
+    end
+  end
 end
