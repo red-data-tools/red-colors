3860d0d96a43ecd6838a379656dbf9e39665d994
Kenta Murata <3959+mrkn@users.noreply.github.com>
2021-12-24 10:40:02 +0900

    Update css3.rb
    - Fix coding style
    - Raise ArgumentError when unknown color space is given
    - Rename CSS3 to CSS for future CSS4 support

--- lib/colors/css3.rb
+++ lib/colors/css3.rb
@@ -1,5 +1,5 @@
 module Colors
-  module CSS3
+  module CSS
 
     # Factory method for generating RGB/RGBA/HSL/HSLA Objects.
     # Parsing based on spec https://www.w3.org/TR/css-color-3 ; section 4.2
@@ -12,46 +12,46 @@ module Colors
 
       css_string = css_string.to_str.strip
 
-      matcher = /\A(rgb|rgba|hsl|hsla)\((.*)\)\z/.match(css_string)
+      matcher = /\A((?:rgb|hsl)a?)\(([^)]+)\)\z/.match(css_string)
       unless matcher
         raise ArgumentError, "#{error_message}: #{css_string.inspect}"
       end
       
-      css_type, argstring = matcher[1..2]
-      arglist = argstring.strip.split(/\s*,\s*/)
-      has_alpha = css_type[-1] == 'a' # rgba/hsla
-      if has_alpha && arglist.length != 4
-        raise ArgumentError, "Expecting 4 fields for #{css_type}(): #{css_string.inspect}"
-      elsif !has_alpha && arglist.length != 3
-        raise ArgumentError, "Expecting 3 fields for #{css_type}(): #{css_string.inspect}"
+      color_space, args_string = matcher[1..2]
+      args = args_string.strip.split(/\s*,\s*/)
+      has_alpha = color_space.end_with?("a") # rgba/hsla
+      if has_alpha && args.length != 4
+        raise ArgumentError, "Expecting 4 fields for #{color_space}(): #{css_string.inspect}"
+      elsif !has_alpha && args.length != 3
+        raise ArgumentError, "Expecting 3 fields for #{color_space}(): #{css_string.inspect}"
       end
       
-      case css_type
-      when /rgb/
-        rgb, alpha = arglist[0..2], arglist[3]
-        num_percent_vals = rgb.count { |v| v[-1] == '%' }
+      case color_space
+      when "rgb", "rgba"
+        rgb, alpha = args[0, 3], args[3]
+        num_percent_vals = rgb.count {|v| v.end_with?("%") }
         # CSS3 allows RGB values to be specified with all 3 as a percentage "##%"
         # or all as integer values without '%' sign.
         if num_percent_vals.zero?
           # interpret as integer values in range of 0..255
-          r, g, b = rgb.map { |strval| strval.to_i.clamp(0, 255) }
+          r, g, b = rgb.map {|strval| strval.to_i.clamp(0, 255) }
           # Note, RGBA.new expects all values to be integers or floats.
           # For this case, we also turn the alpha-value into an int range 0..255
           # to match the r,g,b values.
           a = has_alpha && (alpha.to_r.clamp(0r, 1r) * 255).to_i
         elsif num_percent_vals == 3
-          r, g, b = rgb.map { |strval| (strval.to_r / 100).clamp(0r, 1r) }
+          r, g, b = rgb.map {|strval| (strval.to_r / 100).clamp(0r, 1r) }
           a = has_alpha && alpha.to_r.clamp(0r, 1r)
         else
-          raise ArgumentError, "Invalid mix of percent and integer values:  #{css_string.inspect}"
+          raise ArgumentError, "Invalid mix of percent and integer values: #{css_string.inspect}"
         end
         return has_alpha ? RGBA.new(r, g, b, a) : RGB.new(r, g, b)
         
-      when /hsl/
-        hue, sat, light, alpha = *arglist
+      when "hsl", "hsla"
+        hue, sat, light, alpha = *args
         # CSS3 Hue values are an angle, unclear if we should convert to Integer or Rational here.
         h = hue.to_r
-        s, l = [sat, light].map { |strval| (strval.to_r / 100).clamp(0r, 1r) }
+        s, l = [sat, light].map {|strval| (strval.to_r / 100).clamp(0r, 1r) }
         if has_alpha
           a = alpha.to_r.clamp(0r, 1r)
           return HSLA.new(h, s, l, a)
@@ -59,7 +59,7 @@ module Colors
           return HSL.new(h, s, l)
         end
       else
-        raise "Illegal condition"
+        raise ArgumentError, "Unknown color space: #{css_string.inspect}"
       end
     end
   end
