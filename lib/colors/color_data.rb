23183f9c9aa67b52c20858ebfc18fbf2362dfd91
nanobowers <nanobowers@gmail.com>
2022-01-28 20:16:48 -0500

    Update lib/colors/color_data.rb
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

--- lib/colors/color_data.rb
+++ lib/colors/color_data.rb
@@ -1,4 +1,4 @@
-require_relative "./color_data/matplotlib"
+require_relative "color_data/matplotlib"
 require_relative "./color_data/tableau"
 require_relative "./color_data/xkcd"
 require_relative "./color_data/css4"
